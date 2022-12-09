bf0576a2fabb20ac23638545cac2372332a333ad
nakano <ynaknao@atworks.co.jp>
2022-12-10 02:21:28 +0900

    スタイルファイルの分割

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './Footer.module.css'
 
 export function Footer() {
   return (
--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css'
+import styles from './Main.module.css'
 import { Links } from '../components/Links'
 import { Headline } from '../components/Headline'
 
