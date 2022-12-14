26b7fb1fbe96106a1fbbaa02b4e6fd8a6a02352b
yukintech <yn.yukintech@gmail.com>
2022-12-15 07:16:15 +0900

    クリックイベントのテスト

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,25 @@ import { Footer } from 'src/components/Footer'
 import styles from 'src/styles/Home.module.css'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
 
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
 
       <Main page="index" />
 
