2b13d51ab88379e53a23f5ee8b759e0e2beaa549
nakano <ynaknao@atworks.co.jp>
2022-12-16 02:50:10 +0900

    useStateで文字列と真偽値を確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   let [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     console.log(count);
@@ -14,6 +16,18 @@ export default function Home() {
       setCount(count => count + 1);
     }    
   }, [count]);
+
+  const handleText = useCallback((e) => {
+    if(e.target.value.length > 5) {
+      alert("5文字以内にしてください")
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = () => {
+    setIsShow((isShow) => !isShow);
+  }
 
   useEffect(() => {
     document.body.style.backgroundColor ="lightblue";
@@ -28,8 +42,14 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input
+          type="text"
+          value={text}
+          onChange={handleText}
+      />
 
       <Main page="index" />
 
