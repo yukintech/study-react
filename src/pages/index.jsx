92127c7f9b51c6fbe0c834dc9d4c203b971f72bb
nakano <ynaknao@atworks.co.jp>
2022-12-15 21:17:43 +0900

    カウンター設置

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,35 +3,32 @@ import { Footer } from 'src/components/Footer'
 import styles from 'src/styles/Home.module.css'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
-  // const foo = 1
+  let [count, setCount] = useState(1);
 
-  // const handleClick = useCallback((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault();
-  //   alert(foo);
-  // }, [])
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント")
     document.body.style.backgroundColor ="lightblue";
     return () => {
-      console.log("アンマウント");
       document.body.style.backgroundColor ="";
     };
   }, []);
 
+  console.log(count)
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
-      {/* <a href="/about" onClick={handleClick}>
-        ボタン
-      </a> */}
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
